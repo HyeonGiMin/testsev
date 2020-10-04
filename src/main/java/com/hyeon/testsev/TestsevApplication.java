7bb180175bea7d45783cbe5cbe353ef8dfde3775
HyeonGi Min <hyeoni1995@gmail.com>
2020-10-04 15:19:00 +0900

    Initial commit

---
+++ src/main/java/com/hyeon/testsev/TestsevApplication.java
@@ -0,0 +1,13 @@
+package com.hyeon.testsev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestsevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestsevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hyeon/testsev/TestsevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyeon.testsev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestsevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
